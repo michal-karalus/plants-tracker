06651c19b549d5998fe70b04b8d8141d001ac72f
kashubian <karalusmichal@gmail.com>
2023-06-07 10:48:20 +0200

    Initial commit

---
+++ apps/frontend/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'plants-tracker-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'frontend';
+}
