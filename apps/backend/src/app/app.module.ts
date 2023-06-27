ca811164f24e7d8791c7c34907aa384513064930
kashubian <karalusmichal@gmail.com>
2023-06-27 21:20:14 +0200

    Remove comments

--- apps/backend/src/app/app.module.ts
+++ apps/backend/src/app/app.module.ts
@@ -2,8 +2,8 @@ import { Module } from '@nestjs/common';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ServeStaticModule } from '@nestjs/serve-static'; // <- INSERT LINE
-import { join } from 'path'; // <- INSERT LINE
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 
 @Module({
   imports: [
