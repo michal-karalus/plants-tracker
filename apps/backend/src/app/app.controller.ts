6048e8effb8174dd2777caf2a90f616980e0b6a3
kashubian <karalusmichal@gmail.com>
2023-07-10 19:23:14 +0200

    Return and add plants for specific plot

--- apps/backend/src/app/app.controller.ts
+++ apps/backend/src/app/app.controller.ts
@@ -1,19 +1,20 @@
-import { Controller, Get, Post, Body } from '@nestjs/common';
+import { Controller, Get, Post, Body, Param } from '@nestjs/common';
 import { Plant } from '@prisma/client';
 
 import { AppService } from './app.service';
 
-@Controller('/plants')
+@Controller(':plotId/plants')
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getData() {
-    return this.appService.getData();
+  getData(@Param('plotId') plotId: string) {
+    return this.appService.getData(Number(plotId));
   }
 
   @Post()
-  addData(@Body() data: Plant) {
+  addData(@Param('plotId') plotId: string, @Body() data: Plant) {
+    data.plotId = Number(plotId);
     return this.appService.addData(data);
   }
 }
--- apps/frontend/src/app/services/plants.service.ts
+++ apps/frontend/src/app/services/plants.service.ts
@@ -10,18 +10,24 @@ import { environment } from '@/environments/environment.development';
 })
 export class PlantsService {
   private plantSize = 20;
+  plotId = 1;
 
   constructor(private httpClient: HttpClient) {}
 
   getPlants(): Observable<Plant[]> {
-    return this.httpClient.get<Plant[]>(`${environment.API_URL}/plants`);
+    return this.httpClient.get<Plant[]>(
+      `${environment.API_URL}/${this.plotId}/plants`
+    );
   }
 
   addPlant(event: MouseEvent): Observable<Plant> {
     const { offsetX, offsetY } = event;
-    return this.httpClient.post<Plant>(`${environment.API_URL}/plants`, {
-      positionX: offsetX - this.plantSize,
-      positionY: offsetY - this.plantSize,
-    });
+    return this.httpClient.post<Plant>(
+      `${environment.API_URL}/${this.plotId}/plants`,
+      {
+        positionX: offsetX - this.plantSize,
+        positionY: offsetY - this.plantSize,
+      }
+    );
   }
 }
