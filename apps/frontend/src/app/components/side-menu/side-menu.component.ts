21004f1c8c66553e933bf2a4bf4b19e44bfedc90
kashubian <karalusmichal@gmail.com>
2023-07-13 20:10:03 +0200

    Move side menu to separate component

---
+++ apps/frontend/src/app/components/side-menu/side-menu.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'plants-tracker-side-menu',
+  templateUrl: './side-menu.component.html',
+  styleUrls: [],
+})
+export class SideMenuComponent {}
