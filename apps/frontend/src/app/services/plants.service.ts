b095f863969744c9066a4b63c0fd22b3c811b37d
michal-karalus <116072067+michal-karalus@users.noreply.github.com>
2023-07-17 18:41:42 +0200

    Feature/frontend envs (#14)
    * Use proper env locally
    
    * Add production env

--- apps/frontend/src/app/services/plants.service.ts
+++ apps/frontend/src/app/services/plants.service.ts
@@ -3,7 +3,7 @@ import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Plant } from '@prisma/client';
 
-import { environment } from '@/environments/environment.development';
+import { environment } from '@/environments/environment';
 
 @Injectable({
   providedIn: 'root',
