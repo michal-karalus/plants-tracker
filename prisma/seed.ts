0c6cfd72fdbeb89323ba772067112dc5ab86136f
michal-karalus <116072067+michal-karalus@users.noreply.github.com>
2023-07-28 10:00:12 +0200

    Add database seed (#15)

---
+++ prisma/seed.ts
@@ -0,0 +1,26 @@
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
+
+async function main() {
+  await prisma.plot.createMany({
+    data: [{ name: 'Garden Plot', id: 1 }],
+  });
+
+  await prisma.plant.createMany({
+    data: [
+      { plotId: 1, positionX: 250, positionY: 200 },
+      { plotId: 1, positionX: 550, positionY: 100 },
+    ],
+  });
+}
+
+main()
+  .then(async () => {
+    await prisma.$disconnect();
+  })
+  .catch(async (e) => {
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
