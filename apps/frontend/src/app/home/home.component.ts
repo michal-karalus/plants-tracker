f5f95b49e847bc7861d1024b2aa4690faa558d87
kashubian <karalusmichal@gmail.com>
2023-08-01 20:20:20 +0200

    Fetch and add plants reactively

--- apps/frontend/src/app/home/home.component.ts
+++ apps/frontend/src/app/home/home.component.ts
@@ -1,38 +1,26 @@
-import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
-import { Plant } from '@prisma/client';
+import { Component } from '@angular/core';
 
 import { PlantsService } from '../services/plants.service';
+import { RouteDataService } from '../services/route-data.service';
 
 @Component({
   selector: 'plants-tracker-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
 })
-export class HomeComponent implements OnInit {
-  plants: Plant[] = [];
-  plotId: string;
+export class HomeComponent {
+  private readonly plotId$ = this.routeData.activeRoutePlotId$;
+
+  plants$ = this.plantsService.plants$;
 
   constructor(
     private plantsService: PlantsService,
-    private route: ActivatedRoute
-  ) {
-    this.plotId = this.route.snapshot.paramMap.get('plotId') ?? '';
-  }
-
-  ngOnInit() {
-    this.fetchPlants();
-  }
+    private routeData: RouteDataService
+  ) {}
 
   addPlant(event: MouseEvent): void {
-    this.plantsService.addPlant(event, this.plotId).subscribe(() => {
-      this.fetchPlants();
-    });
-  }
-
-  private fetchPlants(): void {
-    this.plantsService.getPlants(this.plotId).subscribe((data) => {
-      this.plants = data;
+    this.plantsService.addPlant(event, this.plotId$).subscribe(() => {
+      this.plantsService.refetch();
     });
   }
 }
--- apps/frontend/src/app/services/plants.service.ts
+++ apps/frontend/src/app/services/plants.service.ts
@@ -1,17 +1,35 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import {
+  BehaviorSubject,
+  Observable,
+  combineLatest,
+  switchMap,
+  take,
+} from 'rxjs';
 import { Plant } from '@prisma/client';
 
 import { environment } from '@/environments/environment';
+import { RouteDataService } from './route-data.service';
 
 @Injectable({
   providedIn: 'root',
 })
 export class PlantsService {
   private plantSize = 20;
+  private readonly plotId$ = this.routeData.activeRoutePlotId$;
+  private readonly refetchPlants$ = new BehaviorSubject(true);
 
-  constructor(private httpClient: HttpClient) {}
+  plants$ = combineLatest([this.plotId$, this.refetchPlants$]).pipe(
+    switchMap(([plotId]) => {
+      return this.getPlants(plotId);
+    })
+  );
+
+  constructor(
+    private httpClient: HttpClient,
+    private routeData: RouteDataService
+  ) {}
 
   getPlants(plotId: string): Observable<Plant[]> {
     return this.httpClient.get<Plant[]>(
@@ -19,14 +37,23 @@ export class PlantsService {
     );
   }
 
-  addPlant(event: MouseEvent, plotId: string): Observable<Plant> {
-    const { offsetX, offsetY } = event;
-    return this.httpClient.post<Plant>(
-      `${environment.API_URL}/${plotId}/plants`,
-      {
-        positionX: offsetX - this.plantSize,
-        positionY: offsetY - this.plantSize,
-      }
+  addPlant(event: MouseEvent, plotId$: Observable<string>): Observable<Plant> {
+    return plotId$.pipe(
+      take(1),
+      switchMap((plotId) => {
+        const { offsetX, offsetY } = event;
+        return this.httpClient.post<Plant>(
+          `${environment.API_URL}/${plotId}/plants`,
+          {
+            positionX: offsetX - this.plantSize,
+            positionY: offsetY - this.plantSize,
+          }
+        );
+      })
     );
+  }
+
+  refetch(): void {
+    this.refetchPlants$.next(true);
   }
 }
