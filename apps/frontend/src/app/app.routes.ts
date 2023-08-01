4017d4191d2a22f95afb6618f76229e1e8c319dc
michal-karalus <116072067+michal-karalus@users.noreply.github.com>
2023-08-01 20:27:06 +0200

    Feature/refactor plants (#18)
    * Add route data service
    
    * Fetch and add plants reactively
    
    * Rename plants component

--- apps/frontend/src/app/app.routes.ts
+++ apps/frontend/src/app/app.routes.ts
@@ -2,11 +2,11 @@ import { Route } from '@angular/router';
 
 import { PlotsComponent } from './plots/plots.component';
 import { PlotFormComponent } from './plot-form/plot-form.component';
-import { HomeComponent } from './home/home.component';
+import { PlantsComponent } from './plants/home.component';
 
 export const appRoutes: Route[] = [
   { path: '', component: PlotsComponent },
   { path: 'plot/new', component: PlotFormComponent },
   { path: 'plot/:plotId/edit', component: PlotFormComponent },
-  { path: 'plot/:plotId', component: HomeComponent },
+  { path: 'plot/:plotId', component: PlantsComponent },
 ];
---
+++ apps/frontend/src/app/plants/home.component.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core';
+
+import { PlantsService } from '../services/plants.service';
+import { RouteDataService } from '../services/route-data.service';
+
+@Component({
+  selector: 'plants-tracker-home',
+  templateUrl: './home.component.html',
+  styleUrls: [],
+})
+export class PlantsComponent {
+  private readonly plotId$ = this.routeData.activeRoutePlotId$;
+
+  plants$ = this.plantsService.plants$;
+
+  constructor(
+    private plantsService: PlantsService,
+    private routeData: RouteDataService
+  ) {}
+
+  addPlant(event: MouseEvent): void {
+    this.plantsService.addPlant(event, this.plotId$).subscribe(() => {
+      this.plantsService.refetch();
+    });
+  }
+}
